a32c8a4cdd31097549e15f6f9b05584268cec2f3
huaidanxiaomo <1983193612@qq.com>
2019-01-06 17:26:53 +0800

    left提交并推送2

--- src/main/java/com/liuyi/controller/BaseController.java
+++ src/main/java/com/liuyi/controller/BaseController.java
@@ -3,7 +3,7 @@ package com.liuyi.controller;
 public class BaseController {
     public static void main(String[] args) {
         System.out.println("lllllllllllll");
-
         System.out.println("rrrrrrrrrrrrrr");
+        System.out.println(1111111111);
     }
 }
